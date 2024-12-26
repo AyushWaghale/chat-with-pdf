4d0fc5b3aa598d49f023de981579b12670a0bfd0
Ichikawa Hiroshi <157710040+0xichikawa@users.noreply.github.com>
2024-12-26 19:25:55 +0800

    [fix] fixed clear_chat_history func

--- app.py
+++ app.py
@@ -84,12 +84,10 @@ os.environ['REPLICATE_API_TOKEN'] = replicate_api
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [{"role": "assistant", "content": "Upload a PDF file from the sidebar to get started."}]
 
-
 # Display or clear chat messages
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-
 
 def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Upload a PDF file from the sidebar to get started."}]
